94dbcf1c7f7cc99f8ea30c7f56d38a49666d5fd0
shevchuk-workgit <96386271+shevchuk-workgit@users.noreply.github.com>
2022-04-05 19:33:16 +0300

    05.04.2022

--- js/modules/page_style.js
+++ js/modules/page_style.js
@@ -3,38 +3,87 @@
 
 function changeBackgroundColor(){
   const container = document.querySelector('.header_container');
-  const colorBlockContainer =document.querySelector('.message_color-block')
-  const messageColorOpacityRange = document.querySelector('#message_color_opacity-range')
+  const colorBlockContainer =document.querySelector('.message_color-block');
+  const messageColorOpacityRange = document.querySelector('#message_color_opacity-range');
+  const textColorOpacity =document.querySelector('#message_color_opacity-range_text');
+  const messageHeaderText = document.querySelector('#message_change-header-text');
+  let currentOpacity = '0.5';
+  let nextOpacity = messageColorOpacityRange.value/100;
+
+  textColorOpacity.value=+currentOpacity*100;
 
 
   colorBlockContainer.addEventListener('click',(colorBlock)=>{
     if (colorBlock.target.dataset.color){
-      // console.log(colorBlock.target.dataset.color);
       container.classList.remove('temporary-light')
       container.style.backgroundColor = colorBlock.target.dataset.color;
-      console.log(container.style.backgroundColor);
 
-      container.style.backgroundColor = container.style.backgroundColor.replace(/0.5/gi,`${messageColorOpacityRange.value/100}`);
-      console.log(messageColorOpacityRange.value)
-      console.log(container.style.backgroundColor);
-      // changeBackgroundColor();
-      // let a = container.style.backgroundColor;
-      // a=a.replace("0.5","1.0");
-      // console.log(a);
+      container.style.backgroundColor = container.style.backgroundColor.replace('0.5',nextOpacity);
 
+      textColorOpacity.value=Math.round(currentOpacity*100);
+      
+      container.style.boxShadow = '5px 5px 5px black';
       }
   })
 
   messageColorOpacityRange.addEventListener('input',()=>{
-    container.style.backgroundColor = container.style.backgroundColor.replace(/0.5/gi,`${messageColorOpacityRange.value/100}`);
-    console.log(messageColorOpacityRange.value);
+    nextOpacity=messageColorOpacityRange.value/100;
+
+    if(nextOpacity==1){
+      nextOpacity='0.99'
+    }else if(nextOpacity==0){
+      nextOpacity='0.01'
+    };
+
+    changeOpacity()
+
+    textColorOpacity.value=Math.round(currentOpacity*100);
   })
 
-  // container.style.backgroundColor = container.style.backgroundColor.replace(/0.5/gi,'1');
+  textColorOpacity.addEventListener('input',()=>{
 
-  // console.log(messageColorOpacityRange.value) ;
+    if( isNaN(textColorOpacity.value)){
+      textColorOpacity.value = ""
+      nextOpacity=1/100;
+      messageColorOpacityRange.value = nextOpacity*100
+      changeOpacity()
+    }else if (textColorOpacity.value<1){
+      nextOpacity=1/100;
+      messageColorOpacityRange.value = nextOpacity*100
+      changeOpacity()
+    }else if(textColorOpacity.value>99){
+      textColorOpacity.value=99
+      nextOpacity=99/100;
+      messageColorOpacityRange.value = nextOpacity*100
+      changeOpacity()
+    } else {
+      nextOpacity = textColorOpacity.value/100
+      messageColorOpacityRange.value = nextOpacity*100
+      changeOpacity()
+    }  
+  })
 
 
+  function changeOpacity (){
+    container.style.backgroundColor = container.style.backgroundColor.replace(currentOpacity,nextOpacity);
+    currentOpacity=nextOpacity;
+  }
+
+  function createHeaderText(){
+    const headerText = document.createElement('div');
+    headerText.classList.add('header_content','header_font-text');
+    container.append(headerText);
+
+
+    messageHeaderText.addEventListener('input',()=>{
+
+      console.log(messageHeaderText.value);
+      headerText.textContent = messageHeaderText.value
+    })
+
+  }
+  
+  createHeaderText()
 }
 
 export {changeBackgroundColor};
